5f537d4a6a0c3c7d5ee1b8fbbbd0f5a7adca656c
Krysmal <malinowskizxcvbnm@gmail.com>
2024-05-05 22:57:10 +0200

    w trakcie pracy

--- CTP.py
+++ CTP.py
@@ -7,6 +7,7 @@ import io
 
 from PIL import Image, ImageTk
 
+data= pd.DataFrame()
 
 class DataVisualizationApp:
     def __init__(self, master):
@@ -15,13 +16,22 @@ class DataVisualizationApp:
         
         self.load_button = tk.Button(master, text="Load Data", command=self.load_data)
         self.load_button.pack(pady=10)
+
+        self.load_button = tk.Button(master, text="Calibrate Sensor", command=self.Calibrate_Sensor)
+        self.load_button.pack(pady=10)
         
         self.canvas = tk.Canvas(master, width=600, height=400)
         self.canvas.pack()
-        
+
+    
+    
+    
+
     def load_data(self):
+        
         file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
         if file_path:
+            
             data = pd.read_csv(file_path)
 
             buffer = io.BytesIO()
@@ -40,6 +50,32 @@ class DataVisualizationApp:
                 time.sleep(0.05)  # Sleep for a short interval
 
             plt.close()
+
+    def Calibrate_Sensor(self):
+        global data
+        if data.empty:
+            file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+            if file_path:
+                data = pd.read_csv(file_path)
+        
+        
+
+        buffer = io.BytesIO()
+        plt.figure(figsize=(6, 4))  # Create the figure outside the loop
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('Data Visualization')
+        plt.tight_layout()
+        self.ylimits = (data['y'].min(), data['y'].max() + data['y'].std())
+        for i in range(0, len(data) - 100, 5):
+            buffer.truncate(0)  # Clear the buffer
+            buffer.seek(0)  # Reset the buffer position
+            self.display_chart(data[i:(i + 100)], buffer)
+            self.canvas.update()  # Update the canvas immediately
+            time.sleep(0.05)  # Sleep for a short interval
+        plt.close()
+        
+
 
 
     def display_chart(self, data, buffer):
